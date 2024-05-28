ea129e29598ea3805bb49cf061817b2ef6e9d099
developer4236 <157145377+developer4236@users.noreply.github.com>
2024-05-28 16:20:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,11 +17,11 @@ Selected_data = (age,annual_salary,credit_card_debt,net_worth)
 
 try:
     if st.button('predict'):
-        reshaped_data = np.asarray(Selected_data, dtype=float).reshape(1,-1)
-	if  '' in reshaped_data:
-             st.warning('Please fill all values')
-        else:   
-             prediction = model.predict(reshaped_data)
-	     st.success('Car Purchase Amout ' + str(prediction[0][0]))
+         reshaped_data = np.asarray(Selected_data, dtype=float).reshape(1,-1)
+	 if  '' in reshaped_data:
+              st.warning('Please fill all values')
+         else:   
+              prediction = model.predict(reshaped_data)
+	      st.success('Car Purchase Amout ' + str(prediction[0][0]))
 except Exception as e:
     st.warning(f'An error occurred: {e}')
