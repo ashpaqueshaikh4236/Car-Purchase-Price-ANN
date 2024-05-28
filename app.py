f8be72d734fd9e3c8507095b8ae183332df7cea4
developer4236 <157145377+developer4236@users.noreply.github.com>
2024-05-28 16:39:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ import streamlit as st
 import numpy as np
 from keras.models import load_model
 
-
 model = load_model('model.keras')
-
 
 st.title("Car Purchase Predictor")
 
@@ -13,16 +11,64 @@ annual_salary = st.text_input('Annual Salary')
 credit_card_debt = st.text_input('Credit Card Debt')
 net_worth = st.text_input('Net Worth')
 	
-Selected_data = (age,annual_salary,credit_card_debt,net_worth)
+Selected_data = (age, annual_salary, credit_card_debt, net_worth)
 
-try:
-    if st.button('predict'):
-        reshaped_data = np.asarray(Selected_data, dtype=float).reshape(1, -1)
-        
-        if '' in reshaped_data:
-            st.warning('Please fill all values')
-        else:
+if st.button('Predict'):
+    if '' in Selected_data:
+        st.warning('Please fill all values')
+    else:
+        try:
+            reshaped_data = np.asarray(Selected_data, dtype=float).reshape(1, -1)
             prediction = model.predict(reshaped_data)
             st.success('Car Purchase Amount ' + str(prediction[0][0]))
-except Exception as e:
-    st.warning(f'An error occurred: {e}')
+        except Exception as e:
+            st.warning(f'An error occurred: {e}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import streamlit as st
+# import numpy as np
+# from keras.models import load_model
+
+
+# model = load_model('model.keras')
+
+
+# st.title("Car Purchase Predictor")
+
+# age = st.text_input('Age')
+# annual_salary = st.text_input('Annual Salary')
+# credit_card_debt = st.text_input('Credit Card Debt')
+# net_worth = st.text_input('Net Worth')
+	
+# Selected_data = (age,annual_salary,credit_card_debt,net_worth)
+
+# try:
+#     if st.button('predict'):
+#         reshaped_data = np.asarray(Selected_data, dtype=float).reshape(1, -1)
+        
+#         if '' in reshaped_data:
+#             st.warning('Please fill all values')
+#         else:
+#             prediction = model.predict(reshaped_data)
+#             st.success('Car Purchase Amount ' + str(prediction[0][0]))
+# except Exception as e:
+#     st.warning(f'An error occurred: {e}')
