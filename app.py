88118b6052a65d4717ba2afa221edbb9558a0cd4
developer4236 <157145377+developer4236@users.noreply.github.com>
2024-06-01 13:03:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,46 +15,14 @@ net_worth = st.text_input('Net Worth')
 	
 Selected_data = (age,annual_salary,credit_card_debt,net_worth)
 
-if st.button('predict'):
-    reshaped_data = np.asarray(Selected_data).reshape(1, -1)
-    st.write(reshaped_data)
-    if '' in reshaped_data:
-        st.warning('Please fill all values')
-    else:
-        prediction = model.predict(reshaped_data)
-        st.success('Car Purchase Amount ' + str(prediction[0]))
-
-
-
-
-
-
-
-
-
-
-# try:
-#     if st.button('predict'):
-#         reshaped_data = np.asarray(Selected_data, dtype=float).reshape(1, -1)
+try:
+    if st.button('predict'):
+        reshaped_data = np.asarray(Selected_data, dtype=float).reshape(1, -1)
 	    
-#         if '' in reshaped_data:
-#             st.warning('Please fill all values')
-#         else:
-#             prediction = model.predict(reshaped_data)
-#             st.success('Car Purchase Amount ' + str(prediction[0]))
-# except Exception as e:
-#     st.warning(f'An error occurred: {e}')
-
-
-
-# try:
-# if st.button('predict'):
-#     reshaped_data = np.asarray(Selected_data).reshape(1, -1)
-
-#     if '' in reshaped_data:
-#         st.warning('Please fill all values')
-#     else:
-#         prediction = model.predict(reshaped_data)
-#         st.success('Car Purchase Amount ' + str(prediction[0][0]))
-# except Exception as e:
-#     st.warning(f'An error occurred: {e}')
+        if '' in reshaped_data:
+            st.warning('Please fill all values')
+        else:
+            prediction = model.predict(reshaped_data)
+            st.success('Car Purchase Amount ' + str(prediction[0]))
+except Exception as e:
+    st.warning(f'An error occurred: {e}'
